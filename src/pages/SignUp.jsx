8307a4d290eae87f66c02c4a3ea95db5b0042680
Bhargav <sathvikgontla2525@gmail.com>
2023-10-16 10:31:37 -0700

    Complete the OAuth Functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
-import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth"
-import {db} from "../firebase"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
-import {toast} from "react-toastify"
-
-
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -16,35 +19,39 @@ export default function SignUp() {
     email: "",
     password: "",
   });
-  const { name,email, password } = formData;
-  const navigate = useNavigate()
+  const { name, email, password } = formData;
+  const navigate = useNavigate();
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
-  async function onSubmit(e){
-    e.preventDefault()
+  async function onSubmit(e) {
+    e.preventDefault();
 
     try {
-      const auth = getAuth()
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
       updateProfile(auth.currentUser, {
-        displayName: name
-      })
+        displayName: name,
+      });
       const user = userCredential.user;
-      const formDataCopy = {...formData}
-      delete formDataCopy.password
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
       formDataCopy.timestamp = serverTimestamp();
 
-      await setDoc(doc(db, "users", user.uid), formDataCopy)
-      toast.success("Sign up was successful")
-      navigate("/");
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      // toast.success("Sign up was successful");
+      // navigate("/");
     } catch (error) {
-      toast.error("Something went wrong with the registration")
+      toast.error("Something went wrong with the registration");
     }
-
   }
   return (
     <section>
@@ -100,7 +107,7 @@ export default function SignUp() {
               <p className="mb-6">
                 Have a account?
                 <Link
-                  to="/sign-up"
+                  to="/sign-in"
                   className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
                 >
                   Sign in
