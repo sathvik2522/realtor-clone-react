4b63cae93906d74bc1f06c95b9455c36725b58c8
Bhargav <sathvikgontla2525@gmail.com>
2023-10-17 13:59:18 -0700

    Complete the Forgot Password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,7 @@
-
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import OAuth from "../components/OAuth";
 
 export default function ForgotPassword() {
@@ -8,6 +9,17 @@ export default function ForgotPassword() {
 
   function onChange(e) {
     setEmail(e.target.value);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
   return (
     <section>
@@ -21,6 +33,7 @@ export default function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
@@ -59,6 +72,7 @@ export default function ForgotPassword() {
               <p className="text-center font-semibold mx-4">OR</p>
             </div>
             <OAuth />
+          </form>
         </div>
       </div>
     </section>
