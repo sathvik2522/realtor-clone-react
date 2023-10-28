021917e63b46217d0c497dbc03389d3ccf65f752
Bhargav <sathvikgontla2525@gmail.com>
2023-10-28 16:38:11 -0700

    Created the front-end for the create listing form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
 import {getAuth, updateProfile} from "firebase/auth"
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { db } from "../firebase";
+import {FcHome} from "react-icons/fc"
 import {
   collection,
   deleteDoc,
@@ -86,6 +87,13 @@ export default function Profile() {
             <p onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">Sign out</p>
           </div>
         </form>
+        <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm  font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'> 
+        <Link to="/create-listing" className='flex justify-center items-center '>
+        <FcHome className='mr-2 text-3xl bg-red-300 rounded-full p-1 border-2'/>
+          Sell or rent your home
+        </Link>
+
+        </button>
       </div>
     </section>
     
