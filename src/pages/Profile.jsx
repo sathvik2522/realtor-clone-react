97ef807e6daa71994a0cc113307a9142da88540c
Bhargav <sathvikgontla2525@gmail.com>
2023-11-03 17:40:05 -0700

    Created the MyListings section to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,11 +15,15 @@ import {
   updateDoc,
   where,
 } from "firebase/firestore";
+import { useEffect } from 'react'
+import ListingItem from "../components/ListingItem";
 
 export default function Profile() {
   const auth = getAuth()
   const navigate = useNavigate();
-  const [changeDetail, setChangeDetail] = useState(false)
+  const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const[formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -57,6 +61,24 @@ export default function Profile() {
     }
 
   }
+  useEffect(()=>{
+    async function fetchUserListings(){
+      const listingRef = collection(db, "listings");
+      const q = query(listingRef, where("userRef", "==", auth.currentUser.uid), orderBy("timestamp","desc"));
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc)=>{
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+
+  }, [auth.currentUser.uid]);
   return (
     <>
     <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -96,6 +118,19 @@ export default function Profile() {
         </button>
       </div>
     </section>
+    <div className='max-w-6xl px-3 mt-6 mx-auto'>
+      {!loading && listings.length > 0 && (
+        <>
+        <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
+        <ul>
+          {listings.map((listing)=>(
+            <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+          ))}
+        </ul>
+        
+        </>
+      )}
+    </div>
     
     </>
   )
