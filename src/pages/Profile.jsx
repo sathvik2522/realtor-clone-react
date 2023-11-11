d900c3c557855ab3fbfd00e25c18705a6eb3686f
Bhargav <sathvikgontla2525@gmail.com>
2023-11-10 17:42:28 -0700

    Added delete functionality to the Mylistings in the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -79,6 +79,21 @@ export default function Profile() {
     fetchUserListings();
 
   }, [auth.currentUser.uid]);
+  async function onDelete(listingID){
+    if(window.confirm("Are you sure you want to delete?")){
+      await deleteDoc(doc(db, "listings", listingID))
+      const updatedListings = listings.filter(
+        (listing)=> listing.id !==listingID
+      );
+      setListings(updatedListings)
+      toast.success("Successfully deleted the listing")
+    }
+    
+
+  }
+  function onEdit(listingID){
+    navigate(`/edit-listing/${listingID}`)
+  }
   return (
     <>
     <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -124,7 +139,10 @@ export default function Profile() {
         <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
         <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
           {listings.map((listing)=>(
-            <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+            <ListingItem key={listing.id} id={listing.id} listing={listing.data}
+            onDelete={()=>onDelete(listing.id)}
+            onEdit={()=>onEdit(listing.id)}
+            />
           ))}
         </ul>
         
